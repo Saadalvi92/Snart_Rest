13b9ed961c2e68ddb3d6c016754bbfebde090a51
Saadalvi92 <53086047+Saadalvi92@users.noreply.github.com>
2021-05-15 04:26:18 +0500

    Login using Data from an Array
    the authentication is implemented and the email and password for the app sent using whatsapp

--- App/Screens/WelcomeImageScreen/WelcomeImageScreen.js
+++ App/Screens/WelcomeImageScreen/WelcomeImageScreen.js
@@ -17,26 +17,32 @@ const validationSchema = yup.object().shape({
   email: yup.string().required().email().label('Email'),
   password: yup.string().required().min(4).label('Password'),
 });
-var options = {
-  method: 'GET',
-  url: 'http://sfreshveg.apishub.xyz/smartrestaurent',
-};
+// var options = {
+//   method: 'GET',
+//   url: 'http://sfreshveg.apishub.xyz/smartrestaurent',
+// };api need
+const login_details = [{Email: 'demo@gmail.com', Password: '1234'}];
 function WelcomeImageScreen({navigation}) {
-  useEffect(() => {
-    axios
-      .request(options)
-      .then(function (response) {
-        if (response.status === 200 && response != null) {
-          console.log(response.data);
-        } else {
-          throw new Error('Empty Data');
-        }
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }, []);
-  const [login_Details, setLoginDetails] = useState([]);
+  // useEffect(() => { done for api but failed due to failed api in postman
+  //   axios
+  //     .request(options)
+  //     .then(function (response) {
+  //       if (response.status === 200 && response != null) {
+  //         console.log(response.data);
+  //       } else {
+  //         throw new Error('Empty Data');
+  //       }
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+  // }, []);
+
+  const Login = Values => {
+    if (Values.email == 'demo@gmail.com' && Values.password == '1234') {
+      navigation.navigate('Login');
+    }
+  };
   return (
     <View style={styles.background}>
       <View style={styles.logoContainer}>
@@ -48,8 +54,7 @@ function WelcomeImageScreen({navigation}) {
         <AppForm
           initialValues={{email: '', password: ''}}
           onSubmit={Values => {
-            console.log(Values);
-            navigation.navigate('Login');
+            Login(Values);
           }}
           validationSchema={validationSchema}>
           <AppFormField
